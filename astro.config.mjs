b4630ca32dcf3dec4b4d204374fec4007e6cc786
jihoo <jihoop0620@gmail.com>
2026-06-19 18:02:47 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'uwulisp',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/uwulisp/uwulisp' }],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'uwulisp',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/uwulisp/uwulisp' }],
+            
+            defaultLocale: 'en',
+            locales: {
+                en: {
+                    label: 'English',
+                    lang: 'en-US',
+                },
+                ko: {
+                    label: '한국어',
+                    lang: 'ko-KR',
+                },
+            },
+        }),
+    ],
 });
